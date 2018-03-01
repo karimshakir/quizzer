874af11f99b52f925d84cc339bea5c4fcc08c3e7
karimshakir <karimshakir99@gmail.com>
2018-03-01 12:56:25 -0600

    first commit

---
+++ question.php
@@ -0,0 +1,69 @@
+<?php include 'database.php'; ?>
+<?php session_start(); ?>
+<?php
+  //Set question number
+  $number = (int) $_GET['n'];
+  
+  /*
+  * Get total questions
+  */
+  $query = "SELECT * FROM `questions`";
+  //Get result
+  $results = $mysqli->query($query) or die($mysqli->error.__LINE__);
+  $total = $results->num_rows;
+    
+  /*
+  * Get Question
+  */
+  $query = "SELECT * FROM `questions`
+        WHERE question_number = $number";
+  //Get result
+  $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+  
+  $question = $result->fetch_assoc();
+  
+  /*
+  * Get Choices
+  */
+  $query = "SELECT * FROM `choices`
+        WHERE question_number = $number";
+  //Get results
+  $choices = $mysqli->query($query) or die($mysqli->error.__LINE__);
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8" />
+  <title>PHP Quizzer</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+<body>
+  <header>
+    <div class="container">
+      <h1>PHP Quizzer</h1>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+      <div class="current">Question <?php echo $question['question_number']; ?> of <?php echo $total; ?></div>
+      <p class="question">
+        <?php echo $question['text']; ?>
+      </p>
+      <form method="post" action="process.php">
+        <ul class="choices">
+          <?php while($row = $choices->fetch_assoc()): ?>
+            <li><input name="choice" type="radio" value="<?php echo $row['id']; ?>" /><?php echo $row['text']; ?></li>
+          <?php endwhile; ?>
+        </ul>
+        <input type="submit" value="Submit" />
+        <input type="hidden" name="number" value="<?php echo $number; ?>" />
+      </form>
+    </div>
+  </main>
+  <footer>
+    <div class="container">
+      Copyright &copy; 2018, PHP Quizzer
+    </div>
+  </footer>
+</body>
+</html>
---
+++ quizzer.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 01, 2018 at 04:33 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quizzer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choices`
+--
+
+CREATE TABLE `choices` (
+  `id` int(11) NOT NULL,
+  `question_number` int(11) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT '0',
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `choices`
+--
+
+INSERT INTO `choices` (`id`, `question_number`, `is_correct`, `text`) VALUES
+(1, 1, 1, 'PHP: Hypertext Preprocessor'),
+(2, 1, 0, 'Private Home Page'),
+(3, 1, 0, 'Personal Home Page'),
+(4, 1, 0, 'Personal Hypertext Preprocessor'),
+(5, 2, 1, 'echo \"Hello World\";'),
+(6, 2, 0, ' \"Hello World\";'),
+(7, 2, 0, 'Document. Write(\"Hello World\");');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_number` int(11) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`question_number`, `text`) VALUES
+(1, 'What does PHP stand for?'),
+(2, 'How do you write \"Hello World\" in PHP?');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `choices`
+--
+ALTER TABLE `choices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `choices`
+--
+ALTER TABLE `choices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
